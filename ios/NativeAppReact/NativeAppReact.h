5d0c464f557b43a281c1f0b846bc0706a41a7740
SrinivasMummidi <bala.koteswara@anywhere.co>
2025-08-06 18:56:34 +0530

    Initial commit
    Generated by rnef@0.8.6.

---
+++ ios/NativeAppReact/NativeAppReact.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for NativeAppReact.
+FOUNDATION_EXPORT double NativeAppReactVersionNumber;
+
+//! Project version string for NativeAppReact.
+FOUNDATION_EXPORT const unsigned char NativeAppReactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NativeAppReact/PublicHeader.h>
---
+++ rnef.config.mjs
@@ -0,0 +1,18 @@
+import { platformIOS } from '@rnef/platform-ios';
+import { platformAndroid } from '@rnef/platform-android';
+import { pluginBrownfieldIos } from '@rnef/plugin-brownfield-ios';
+import { pluginBrownfieldAndroid } from '@rnef/plugin-brownfield-android';
+import { pluginMetro } from '@rnef/plugin-metro';
+
+export default {
+  plugins: [
+    pluginBrownfieldIos(),
+    pluginBrownfieldAndroid(),
+  ],
+  bundler: pluginMetro(),
+  platforms: {
+    ios: platformIOS(),
+    android: platformAndroid(),
+  },
+  remoteCacheProvider: null,
+};
